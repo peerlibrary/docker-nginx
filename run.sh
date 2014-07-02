24a45891565a330eddf7253fecdcfafe4edbd7ef
Mitar <mitar.git@tnode.com>
2014-07-02 11:30:58 -0700

    Made image into basic Nginx installation.

--- run.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash -e
-
-docker run -d -p 80:80/tcp --name web -v /var/run/docker.sock:/docker.sock -v /srv/var/log/nginx:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen cloyne/nginx
